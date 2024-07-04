b1b79b266d649fc0686fbcc1f1784d73704a99de
amamyyyyy <amemiyatech@gmail.com>
2024-07-04 19:37:16 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shopping_fee
+  belongs_to :prefecture
+  belongs_to :shopping_day
   belongs_to :user
   has_one_attached :image
 end
