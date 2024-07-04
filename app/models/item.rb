6f0e60e455e5cb2e6a33d8f32fc2e23abb62a685
amamyyyyy <amemiyatech@gmail.com>
2024-07-04 19:47:37 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,13 @@ class Item < ApplicationRecord
   belongs_to :shopping_day
   belongs_to :user
   has_one_attached :image
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shopping_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shopping_day_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 end
