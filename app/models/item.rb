192ac6c881a6d2e7c8b9c26e2a7232af0fd0a51a
amamyyyyy <amemiyatech@gmail.com>
2024-07-04 20:26:36 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,13 +7,7 @@ class Item < ApplicationRecord
   belongs_to :shopping_day
   belongs_to :user
   has_one_attached :image
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shopping_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shopping_day_id, presence: true
-  validates :price, presence: true
-  validates :image, presence: true
+  validates :name, :description, :category_id, :condition_id, :shopping_fee_id, :prefecture_id, :shopping_day_id, :price, :image, presence: true
+  validates :category_id, :condition_id, :shopping_fee_id, :prefecture_id, :shopping_day_id, numericality: { other_than: 1 }
+  
 end
