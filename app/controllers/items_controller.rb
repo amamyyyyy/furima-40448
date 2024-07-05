97931c52ac54c706631b7f298a93558f912f8597
amamyyyyy <amemiyatech@gmail.com>
2024-07-05 11:18:59 +0900

    出品した商品が登録される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,15 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-  
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :shopping_fee_id, :prefecture_id, :shopping_day_id, :price).merge(user_id: current_user.id)
+  end
 end
