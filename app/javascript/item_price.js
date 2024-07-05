daf51a9545512ec8a578777d6faa153492083fa6
amamyyyyy <amemiyatech@gmail.com>
2024-07-05 14:50:39 +0900

    必要なvalue属性を取得

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,7 +1,8 @@
 window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    console.log("イベント発火");
+    const inputValue = priceInput.value;
+    console.log(inputValue);
   })
   
 })
