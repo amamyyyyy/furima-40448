9773e107aff29b93df8bad08e98664b60ee4c701
amamyyyyy <amemiyatech@gmail.com>
2024-07-19 17:18:56 +0900

    フォームの情報を保存できるようにする

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,5 +1,21 @@
 class BuysController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @buy_address = BuyAddress.new(buy_params)
+    if @buy_address.valid?
+      @buy_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def buy_params
+    params.require(:buy_address).permit(:code, :prefecture_id, :city, :street, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,16 +1,16 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :code, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id, :buy_id
+  attr_accessor :code, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :code, :city, :street, :phone_number
+    validates :code, :city, :street, :phone_number, :user_id
     validates :phone_number, format: { with: /\A\d+\z/, message: 'は半角数字で入力してください' }
   end
   validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
     buy = Buy.create(item_id: item_id, user_id: user_id)
-    Address.create(code: code, prefecture_id: prefecture_id, city: city, street: street, building: building, buy_id: buy.id)
+    Address.create(code: code, prefecture_id: prefecture_id, city: city, street: street, building: building, phone_number: phone_number, buy_id: buy.id)
   end
 end
