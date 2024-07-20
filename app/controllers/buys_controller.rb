c57329e04c0ec2e7b2193473e3b50605bddbf9a3
amamyyyyy <amemiyatech@gmail.com>
2024-07-20 18:05:58 +0900

    決済機能を実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -8,6 +8,12 @@ class BuysController < ApplicationController
     @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new(buy_params)
     if @buy_address.valid?
+      Payjp.api_key = "sk_test_97ef9b9af9002e7560c9e603"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price, # 商品の価格
+        card: buy_params[:token], # カードトークン
+        currency: 'jpy' # 通貨の種類（日本円）
+      )
       @buy_address.save
       redirect_to root_path
     else
@@ -15,7 +21,9 @@ class BuysController < ApplicationController
     end
   end
 
+  private
+
   def buy_params
-    params.require(:buy_address).permit(:code, :prefecture_id, :city, :street, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:buy_address).permit(:code, :prefecture_id, :city, :street, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
   end
 end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,10 +1,10 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :code, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id
+  attr_accessor :code, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :city, :street, :user_id, :item_id
+    validates :city, :street, :user_id, :item_id, :token
     validates :phone_number, format: { with: /\A\d+\z/, message: 'は半角数字で入力してください' }
   end
   validates :prefecture_id, numericality: { other_than: 1 }
