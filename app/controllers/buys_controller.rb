b8b0769ddcdac9f8d3db076e49f574a894c1acce
amamyyyyy <amemiyatech@gmail.com>
2024-07-20 18:28:54 +0900

    鍵情報を環境変数として設定する

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,5 +1,6 @@
 class BuysController < ApplicationController
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new
   end
@@ -8,15 +9,11 @@ class BuysController < ApplicationController
     @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new(buy_params)
     if @buy_address.valid?
-      Payjp.api_key = "sk_test_97ef9b9af9002e7560c9e603"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-      Payjp::Charge.create(
-        amount: @item.price, # 商品の価格
-        card: buy_params[:token], # カードトークン
-        currency: 'jpy' # 通貨の種類（日本円）
-      )
+      pay_item
       @buy_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -25,5 +22,14 @@ class BuysController < ApplicationController
 
   def buy_params
     params.require(:buy_address).permit(:code, :prefecture_id, :city, :street, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price, # 商品の価格
+        card: buy_params[:token], # カードトークン
+        currency: 'jpy' # 通貨の種類（日本円）
+      )
   end
 end
